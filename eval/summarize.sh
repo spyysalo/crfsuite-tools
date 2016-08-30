696c3cf328a9789d218b8f259c150ce53b3b2990
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2016-08-30 20:22:40 +0200

    Sort output

--- eval/summarize.sh
+++ eval/summarize.sh
@@ -3,4 +3,4 @@
 # Summarize evaluation results from output.
 
 echo $'dataset\taccuracy\tf-score'
-egrep 'accuracy:' "$@" | perl -pe 's/^(.*?):\s.*?\baccuracy:\s+(\S+)\%.*FB1:\s+(\S+)/$1\t$2\t$3/'
+egrep 'accuracy:' "$@" | perl -pe 's/^(.*?):\s.*?\baccuracy:\s+(\S+)\%.*FB1:\s+(\S+)/$1\t$2\t$3/' | sort
